a3415f6633e04945db6c492c43cfca77d0ed3dfc
MC IT Officer <itoff@adoracruises.com>
2026-07-31 15:41:24 +0900

    Create initial applicationm skeleton

--- app/src/main/java/com/example/apfs_explorer/MainActivity.kt
+++ app/src/main/java/com/example/apfs_explorer/MainActivity.kt
@@ -3,45 +3,18 @@ package com.example.apfs_explorer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.apfs_explorer.ui.screens.HomeScreen
 import com.example.apfs_explorer.ui.theme.APFSExplorerTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             APFSExplorerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    APFSExplorerTheme {
-        Greeting("Android")
     }
 }
